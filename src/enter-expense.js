5342d03c81eee8adf6ea345dd663bfa37b0c49a0
Nicolas Fédou <nicolas.fedou@gmail.com>
2020-02-28 14:14:38 +0100

    removing node_modules from code source

--- src/enter-expense.js
+++ src/enter-expense.js
@@ -3,7 +3,9 @@ const AWS = require('aws-sdk');
 const db = new AWS.DynamoDB.DocumentClient();
 const tableName = process.env.EXPENSESTABLENAME;
 
-exports.handler = async ({ body:{issuer, expenseDate, description, amount, currency, location}}) => {
+exports.handler = async ({ body }) => {
+    // body is a string that must be JSONfied
+    JSON.parse(body):{issuer, expenseDate, description, amount, currency, location};
     console.log(""+issuer+", "+expenseDate+", "+description+", "+amount+", "+currency+", "+location);
     await db.put({
         TableName: tableName,
